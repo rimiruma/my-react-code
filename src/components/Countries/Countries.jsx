cb7aea907f1af474af5d012aa1f6cb5659e16783
rimiruma <rimiruma12@gmail.com>
2025-09-19 21:49:49 +0600

    added country file and condisonal rendaring done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import "./Countries.css"
 
 const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise);
@@ -9,7 +10,7 @@ const Countries = ({ countriesPromise }) => {
     return (
         <div>
            <h1>In The Countries: {countries.length}</h1> 
-           <div>
+           <div className='countries'>
             {
                 countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
             }
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import "./Country.css"
 
 const Country = ({country}) => {
     console.log(country);
+    const handleVisited = () => {
+        console.log('button clicked');
+        
+    }
     
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
            <h3>Name: {country.name.common}</h3>
            <p>Population: {country.population.population}</p> 
+           <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : 'small country'}</p>
+           <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
