3c86e8dc029e489ded256f9df2438fffc716420e
rimiruma <rimiruma12@gmail.com>
2025-09-19 22:19:12 +0600

    added button even handler done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import "./Country.css"
 
 const Country = ({country}) => {
     console.log(country);
+            const [visited, setVisited] =useState(false)
+
     const handleVisited = () => {
-        console.log('button clicked');
-        
+        // console.log('button clicked');
+        if(visited){
+            setVisited(false)
+        }
+        else{
+            setVisited(true)
+        }
     }
     
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
            <h3>Name: {country.name.common}</h3>
            <p>Population: {country.population.population}</p> 
            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : 'small country'}</p>
-           <button onClick={handleVisited}>Not Visited</button>
+           <button onClick={handleVisited}>
+            {visited ? 'Visited' : 'Not Visited'}
+           </button>
         </div>
     );
 };
